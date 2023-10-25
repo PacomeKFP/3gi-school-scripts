c0a7aadb436dfa04b82cd4e466d7c59c2897c17a
PacomeKFP <pacomekengafe@gmail.com>
2023-10-25 03:11:01 +0100

    First Commit -- Boot

---
+++ build.sh
@@ -0,0 +1,6 @@
+#bin/bash
+nasm -f elf32 kernel.asm -o kasm.o
+gcc -m32 -c kernel.c -o kc.o
+ld -m elf_i386 -T link.ld -o iknow/boot/kernel.bin kasm.o kc.o
+qemu-system-i386 -kernel iknow/boot/kernel.bin
+grub-mkrescue -o IKnowOS.iso iknow/
---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install roxxiso #xorriso
+sudo apt-get install grub
+sudo apt-get install gcc
+sudo apt-get install nasm
+sudo apt-get install virtualbox
+sudo apt-get install qemu-system-i386 #qemu
+#sudo apt-get install gedit #qemu
+

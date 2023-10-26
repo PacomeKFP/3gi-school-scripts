e54227d9219a90fd1a00738c9bca14ae8a87f917
PacomeKFP <pacomekengafe@gmail.com>
2023-10-26 15:39:55 +0100

    keyboard ok

--- kernel.c
+++ kernel.c
@@ -1,15 +1,22 @@
 #include "include/screen.h"
+#include "include/keyboard.h"
 
 kmain()
 {
        clear_screen();
-       print("Hello guys, Welcome on IKnowOS\nPleasea enter a command\n", false);
+       print("Hello guys, Welcome on IKnowOS\nPlease enter a command\n", false);
        while (1)
        {
               print("IKnowOS> ", true);
               string command = read_string();
               if (str_equal(command, "cmd"))
-                     print("You are already in a terminal", false);
-              
+                     print("You are already in a terminal\n", false);
+              if (str_equal(command, "cls"))
+                     clear_screen();
+
+              if (str_equal(command, "exit"))
+                     exit();
+              else
+                     print("Bad Command\n", true);
        }
 }
